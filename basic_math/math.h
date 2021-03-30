8fed7cb4fe3015256ec90e53c65cbd0b83ea9c18
a3569118344bf <ff4810e550f5c@50be40e40.98e0fa96>
2021-03-30 22:40:49 +0000

    added to do cont tmrw

---
+++ basic_math/math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_CRYPTO
+#define MATH_CRYPTO
+
+// define error enum
+
+extern int mod(int dividend, int divisor);			// get remainder
+extern int div(int dividend, int divisor);			// get quotient
+
+#endif /* MATH_CRYPTO */
--- basic_math/modulo_aritmetic.c
+++ basic_math/modulo_aritmetic.c
@@ -1,3 +1,11 @@
+/* 
+ * TO DO
+ * negative num modulo and division
+ * combine mod and quotiet w/ malloc
+ * error enum in header, better formatting
+ * divide by zero take care
+ */
+
 /*
  * divisor   : the int dividing the dividend
  * dividend  : the int being divided
@@ -14,16 +22,25 @@
 #include <stdio.h>
 
 /* returns remainder after divisor divides dividend */
-int mod (int dividend, int divisor) {
-	int quotient = 0, remainder = dividend;			// for future use
+int mod(int dividend, int divisor) {
+	if (divisor == 0) return -1;				// divide by zero error
 	if (dividend < divisor) return dividend;
 	if (dividend == divisor) return 0;
 	while (dividend >= divisor) {
 		dividend -= divisor;				// dividing (subtracting by the num)
+	}
+	return dividend;
+}
+
+int div(int dividend, int divisor) {
+	int quotient;
+	if (dividend < divisor) return 0;
+	if (dividend == divisor) return 1;
+	while (dividend >= divisor) {
+		dividend -= divisor;				// dividing (subtracting by the num)
 		quotient += 1;
 	}
-	remainder = dividend;					// for future use
-	return dividend;
+	return dividend == 0 ? quotient : 0;			// doesnt divide if still remainder
 }
 
 int main(){
@@ -33,5 +50,7 @@ int main(){
 	printf("Enter divisor: ");
 	scanf("%d", &divisor);
 
-	printf("%dmod%d = %d\n", dividend, divisor, mod(dividend, divisor));
+	printf("%d divided by %d\n", dividend, divisor);
+	printf("Quotient  = %d\n", div(dividend, divisor));
+	printf("Remainder =  %d\n", mod(dividend, divisor));
 }
