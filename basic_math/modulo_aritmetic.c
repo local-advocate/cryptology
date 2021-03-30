1aa23ebd73e50df1fc4f9bffec713830d28da0e9
a3569118344bf <ff4810e550f5c@50be40e40.98e0fa96>
2021-03-30 22:23:41 +0000

    one more check divid == divis

--- basic_math/modulo_aritmetic.c
+++ basic_math/modulo_aritmetic.c
@@ -11,11 +11,13 @@
  * n = divisor, q = quotient, d = dividend, r = remainder
  */
 
+#include <stdio.h>
 
 /* returns remainder after divisor divides dividend */
 int mod (int dividend, int divisor) {
 	int quotient = 0, remainder = dividend;			// for future use
 	if (dividend < divisor) return dividend;
+	if (dividend == divisor) return 0;
 	while (dividend > divisor) {
 		dividend -= divisor;				// dividing (subtracting by the num)
 		quotient += 1;
@@ -24,3 +26,12 @@ int mod (int dividend, int divisor) {
 	return dividend;
 }
 
+int main(){
+	int dividend, divisor;
+	printf("Enter dividend: ");
+	scanf("%d", &dividend);
+	printf("Enter divisor: ");
+	scanf("%d", &divisor);
+
+	printf("%dmod%d = %d\n", dividend, divisor, mod(dividend, divisor));
+}
